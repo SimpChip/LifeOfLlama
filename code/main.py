7169111ceb6b7740e738eed0ec3ebc6d702b5fef
SimpChip <mellaendre@gmail.com>
2024-09-18 22:58:24 +0200

    added more rooms and the possibility to move between them, thank you sigma

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 from settings import *
 
-
+import math
 from sigma import Sigma
 from pytmx.util_pygame import load_pygame
 from sprites import Sprite
@@ -17,7 +17,20 @@ class Game:
         self.tick = 0
 
 
-        self.grid = np.zeros((16,16), dtype=int)
+        self.current_room_index = 0
+        self.room_maps = ["map1.tmx", "map2.tmx", "map3.tmx" , "house.tmx"]
+        self.rooms = []
+        self.rooms_exits = {
+            0: {1: [15, 4]},
+            1: {0: [0, 4], 2: [15, 4]},
+            2: {1: [0, 4], 3: [10,6]},
+            3: {2: [4, 15]}
+        }
+
+        
+
+        self.set_up_rooms()
+
         
         # Zoom
         self.zoom_level = -6  # Start with normal size (100% zoom)
@@ -32,53 +45,67 @@ class Game:
         self.sigma_sprites = pygame.sprite.Group()
 
         
-        self.tile_border_enabled = True
+        self.tile_border_enabled = False
 
         self.players = []
-        self.setup()
         
+       
+        
+        self.draw_room(self.room_maps[self.current_room_index])
 
-        
+        self.sigma = Sigma("sigma1.csv", self.all_sprites)
+        self.sigma_sprites.add(self.sigma)
 
         self.world_surface = pygame.Surface((TILE_SIZE * 16 ,  TILE_SIZE * 16))
 
-    def setup(self):
-        map = load_pygame(join('data', 'maps', 'map.tmx'))
 
-        for x, y, image in map.get_layer_by_name('GROUND').tiles():
-            tile_surface = pygame.Surface((TILE_SIZE, TILE_SIZE), pygame.SRCALPHA)
-            tile_surface.blit(image, (0, 0))
-            
-            # Conditionally draw the border around the tile based on the flag
-            if self.tile_border_enabled:
-                border_color = (0, 0, 0, 255)  # Black color for the border
-                border_thickness = 1  # Thickness of the border
-                pygame.draw.rect(tile_surface, border_color, tile_surface.get_rect(), border_thickness)
-            
-            # Create the sprite using this surface
-            Sprite((x * TILE_SIZE + 32, y * TILE_SIZE + 32), tile_surface, self.all_sprites)
+    def set_up_rooms(self):
+        # Load each map and prepare rooms
+        for map_file in self.room_maps:
+            self.set_up_room(map_file)
 
+
+    def set_up_room(self, map_file):
+        map = load_pygame(join('data', 'maps', map_file))
+        grid = np.ones((16, 16))
         for x, y, image in map.get_layer_by_name('OBJECT').tiles():
+            grid[y, x] = 0
+        self.rooms.append(grid)
 
-            self.grid[y,x] = 1
+    def draw_room(self, map_file):
+        # Clear current sprites
+        self.all_sprites.empty()
+
+        # Draw room from map file
+        map = load_pygame(join('data', 'maps', map_file))
+        self.draw_tiles("GROUND", map)
+        self.draw_tiles("OBJECT", map)
+
+
+
+
+    def draw_tiles(self, type, map):
+        for x, y, image in map.get_layer_by_name(type).tiles():
             tile_surface = pygame.Surface((TILE_SIZE, TILE_SIZE), pygame.SRCALPHA)
             tile_surface.blit(image, (0, 0))
 
             if self.tile_border_enabled:
-                border_color = (0, 0, 0, 255)  # Black color for the border
-                border_thickness = 1  # Thickness of the border
+                border_color = (0, 0, 0, 255)
+                border_thickness = 1
                 pygame.draw.rect(tile_surface, border_color, tile_surface.get_rect(), border_thickness)
       
-            # Create the sprite using this surface
             Sprite((x * TILE_SIZE + 32, y * TILE_SIZE + 32), tile_surface, self.all_sprites)
 
+    def switch_room(self, direction):
+        # Switch to the next or previous room based on direction
+        if direction == "next":
+            self.current_room_index = (self.current_room_index + 1) % len(self.room_maps)
+        elif direction == "previous":
+            self.current_room_index = (self.current_room_index - 1) % len(self.room_maps)
 
-        for _ in range(100):  # Example: creating 20 Sigma objects
-            sigma = Sigma([1,1] , self.all_sprites)
-            self.sigma_sprites.add(sigma)
+        # Draw the new room
+        self.draw_room(self.room_maps[self.current_room_index])
 
-
-        print(self.grid)
 
     def handle_zoom(self):
         keys = pygame.key.get_pressed()
@@ -100,13 +127,42 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:  # Left mouse button
+                        # Get the global mouse position
+                        global_mouse_pos = pygame.mouse.get_pos()
+                        
+                        # Calculate the position to center the scaled surface on the screen
+                        scaled_width = int(TILE_SIZE * 16 + 64 * self.zoom_level)
+                        scaled_height = int(TILE_SIZE * 16 + 64 * self.zoom_level)
+                        scaled_surface = pygame.transform.smoothscale(
+                            self.world_surface,
+                            (scaled_width, scaled_height)
+                        )
+                        scaled_rect = scaled_surface.get_rect(center=self.screen.get_rect().center)
+
+                        # Calculate the mouse position relative to the world_surface
+                        relative_x = (global_mouse_pos[0] - scaled_rect.x) * (self.world_surface.get_width() / scaled_rect.width)
+                        relative_y = (global_mouse_pos[1] - scaled_rect.y) * (self.world_surface.get_height() / scaled_rect.height)
+                
+
+                        # Use relative coordinates for whatever action you want
+                        self.sigma.create_directions_to_point(self.rooms, self.rooms_exits, self.current_room_index, [math.floor(relative_x/64), math.floor(relative_y/64)])
+
+                elif event.type == pygame.KEYDOWN:        
+                    if event.key == pygame.K_n:  # Press 'N' to go to the next room
+                        self.switch_room("next")
+                    elif event.key == pygame.K_p:  # Press 'P' to go to the previous room
+                        self.switch_room("previous")
             self.handle_zoom()
 
             #updatet
-            self.all_sprites.update(dt, self.grid)
+            self.all_sprites.update(dt, self.rooms[self.current_room_index])
+            self.sigma_sprites.update(dt, self.rooms[self.current_room_index])
 
             #draw
             self.world_surface.fill('black')
+
 
 
             for sprite in self.all_sprites:
@@ -115,7 +171,8 @@ class Game:
 
             sorted_sigma_sprites = sorted(self.sigma_sprites, key=lambda sprite: sprite.rect.y)
             for sprite in sorted_sigma_sprites:
-                self.world_surface.blit(sprite.image, sprite.rect)
+                if sprite.current_room == self.current_room_index:
+                    self.world_surface.blit(sprite.image, sprite.rect)
 
 
                 
